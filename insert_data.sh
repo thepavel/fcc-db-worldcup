46f5bc661b1559f63e20cd39b4670179f42995af
Pavel Lutskovsky <lutskovp@gmail.com>
2024-08-06 07:03:20 +0000

    wip: fixing issues with parsing of row

--- insert_data.sh
+++ insert_data.sh
@@ -10,43 +10,63 @@ fi
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
 GetTeamId() {
-  
+
   TEAM_NAME=$1
-  get_team_query="SELECT team_id FROM teams WHERE name='$TEAM_NAME'"
-  echo $get_team_query
   TEAM_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$TEAM_NAME'")"
-  echo $TEAM_ID
+
 
   if [[ -z $TEAM_ID ]] # if team_id is empty, team doesn't exist.
   then 
     # insert a new team
     insert_team_query="INSERT INTO teams(name) VALUES('$TEAM_NAME')"
-    insert_team_result="$($PSQL $insert_team_query)"
-    echo $insert_team_result
+    insert_team_result="$($PSQL "$insert_team_query")"
 
-    if [[ $insert_team_result="INSERT 0 1" ]]
+    if [[ $insert_team_result = "INSERT 0 1" ]]
     then
-      echo inserted, querying
-      TEAM_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$TEAM_NAME'")"
-    else 
-      echo $insert_team_result
+      # new team inserted
+      TEAM_ID="$($PSQL "$get_team_query")"
     fi
-
   fi
   # return $1;
 }
 
 skip_headers=1
-while IFS=, read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+IFS=$'\n'
+for row in $(cat games.csv)
 do
-    if ((skip_headers))
-    then
-        ((skip_headers--))
-    else
-        GetTeamId $WINNER
-        echo $TEAM_ID
-        echo "I got:$YEAR|$ROUND|$WINNER|$OPPONENT"
-        
+  if ((skip_headers))
+  then
+    ((skip_headers--))
+  else
+    echo '=============================================='
+    echo "Game Row: $row"
+    echo '----------------------------------------------'
 
-    fi
-done < games.csv
+    IFS=','
+    read -r -a game <<< $row
+
+    winner=${game[2]}
+
+    echo "    Year: ${game[0]}"
+    echo "    Round: ${game[1]}"
+    echo "    Winner: $winner"
+    echo "    Opponent: ${game[3]}"
+    echo "    Score: ${game[4]}:${game[5]}"    
+  fi
+done
+
+# skip_headers=1
+# while IFS="," read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+# do
+#     if ((skip_headers))
+#     then
+#         ((skip_headers--))
+#     else
+#         GetTeamId $WINNER
+#         WINNER_ID=$TEAM_ID
+
+#         GetTeamId $OPPONENT
+#         OPP_ID=$TEAM_ID
+#         echo "I got:$YEAR|$ROUND|$WINNER|$OPPONENT"
+#     fi
+# done < games.csv
